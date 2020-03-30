f103aa023cc550ceb3a56e57ed0b613f4d4646fa
LuukFTF <lucasvdvegt@gmail.com>
2020-03-30 12:45:03 +0200

    Updated colorpicker and added mobile css

--- includes/background.php
+++ includes/background.php
@@ -1,4 +1,5 @@
-<div class="background">
-    <img src="">
-    background
-</div class="background">
+<!--<div class="background">-->
+<!--    <img src="../img/back1.png">-->
+<!--    background-->
+<!--</div class="background">-->
+<div class="background"></div>
