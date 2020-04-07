3b2896f89b401122e58d34116d8e28d75b56f384
LuukFTF <lucasvdvegt@gmail.com>
2020-04-07 19:56:33 +0200

    last version

--- live/colorpicker/index.php
+++ live/colorpicker/index.php
@@ -31,6 +31,22 @@
     </form>
 
 
+
+    <div class="colorpicker-container">
+        <a href="red" class="button2 color" style="background-color: red"></a>
+        <a href="purple" class="button2 color" style="background-color: purple"></a>
+        <a href="dark-blue" class="button2 color" style="background-color: darkblue"></a>
+        <a href="blue" class="button2 color" style="background-color: blue"></a>
+        <a href="cyan" class="button2 color" style="background-color: cyan"></a>
+        <a href="teal" class="button2 color" style="background-color: teal"></a>
+        <a href="green" class="button2 color" style="background-color: green"></a>
+        <a href="yellow" class="button2 color" style="background-color: yellow"></a>
+        <a href="orange" class="button2 color" style="background-color: orange"></a>
+    </div>
+
+
+
+
 </main>
 
 <!--footer-->
